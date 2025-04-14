b30f11af37b486c451fcc36ca0ceb2f4a3ee2eb8
longn16 <lnguyen@deputy.com>
2025-04-14 19:46:06 +0700

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
 
   return (
     <>
+      <h3>Welcome to my website</h3>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
